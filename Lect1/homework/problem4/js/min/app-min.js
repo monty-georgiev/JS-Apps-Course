cfe239356df500ac7208ce7be5a2269474fbe05a
monty-georgiev <monty.georgiev@gmail.com>
2016-02-26 15:19:18 +0200

    Slider
    Some additional styles added. Todo: js functionality;

--- Lect1/homework/problem4/js/min/app-min.js
+++ Lect1/homework/problem4/js/min/app-min.js
@@ -1,7 +1,18 @@
 ;
 (function ($, window, undefined) {
 
+    var $prevBtn = $('#prev-image');
+    var $nextBtn = $('#next-image');
+    var $slides = $('.slide');
 
+
+    $prevBtn.on('click', function () {
+        console.log('previous image');
+    });
+
+    $nextBtn.on('click', function () {
+        console.log('next image');
+    })
 
 
 })(jQuery, window);

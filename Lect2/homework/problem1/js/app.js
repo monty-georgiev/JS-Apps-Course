22d85919290d17b208045eea0d8d60e67e415f57
monty-georgiev <monty.georgiev@gmail.com>
2016-02-28 21:37:11 +0200

    Lecture 2, problem 1
    Lecture 2, problem 1 solved; Solution added to repo;

---
+++ Lect2/homework/problem1/js/app.js
@@ -0,0 +1,40 @@
+;
+(function () {
+
+
+    var usernamePlaceholder = document.getElementById('username');
+    var visitsHeading = document.getElementsByClassName('visits');
+    var totalvisitCountPlaceholder = document.getElementById('total-visits');
+    var sessionvisitCountPlaceholder = document.getElementById('session-visits');
+
+
+    if (!localStorage.getItem('name')) {
+        var prompt = window.prompt('What is your name?');
+
+        if (!prompt) {
+            return;
+        } else {
+            localStorage.setItem('name', prompt);
+            localStorage.setItem('total-visits', 0);
+            sessionStorage.setItem('session-visits', 0);
+            window.location = window.location;
+        }
+
+    } else {
+        var totalVisitsCount = localStorage.getItem('total-visits');
+        var sessionVisitsCount = sessionStorage.getItem('session-visits');
+        totalVisitsCount++;
+        sessionVisitsCount++;
+        localStorage.setItem('total-visits', totalVisitsCount);
+        sessionStorage.setItem('session-visits', sessionVisitsCount);
+        usernamePlaceholder.innerText = localStorage.getItem('name');
+        totalvisitCountPlaceholder.innerText = totalVisitsCount;
+        sessionvisitCountPlaceholder.innerText = sessionVisitsCount;
+
+        for (var i = 0; i < visitsHeading.length; i++) {
+            visitsHeading[i].style.display = 'block';
+        }
+
+    }
+
+})();

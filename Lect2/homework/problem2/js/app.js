e0245bbcc92355392bffbb3b21383c0e6025f4bc
monty-georgiev <monty.georgiev@gmail.com>
2016-02-29 18:02:41 +0200

    Homework2
    Basic structure for problem2 added;

---
+++ Lect2/homework/problem2/js/app.js
@@ -0,0 +1,49 @@
+;
+(function (window, undefined) {
+
+    var questions = [
+        {
+            title: 'Question one',
+            answers: [
+                'answer1',
+                'answer2',
+                'answer3',
+                'answer3'
+            ]
+        },
+        {
+            title: 'Question two',
+            answers: [
+                'answer1',
+                'answer2',
+                'answer3',
+                'answer3'
+            ]
+        }, {
+            title: 'Question three',
+            answers: [
+                'answer1',
+                'answer2',
+                'answer3',
+                'answer3'
+            ]
+        }, {
+            title: 'Question four',
+            answers: [
+                'answer1',
+                'answer2',
+                'answer3',
+                'answer3'
+            ]
+        }
+    ];
+
+
+    var timer = setInterval(function () {
+        console.log('tick');
+    }, 1000);
+
+    console.log(questions)
+
+
+})(window);

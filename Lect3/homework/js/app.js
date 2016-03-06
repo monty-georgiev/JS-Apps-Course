0f797e9a3970364e7f9fd512b1b32ab286f9ec42
monty-georgiev <monty.georgiev@gmail.com>
2016-03-06 20:15:14 +0200

    Geography
    ;Added functionality for countries rendering;

--- Lect3/homework/js/app.js
+++ Lect3/homework/js/app.js
@@ -1,6 +1,63 @@
 ;
 (function ($, window, undefined) {
 
+    var defaults = {
+        baseURL: 'https://baas.kinvey.com',
+        appID: 'kid_ZkWqjeGpAl',
+        username: 'geography',
+        pass: 'geography123',
+        showCountriesButton: '#show-countries',
+        addCountryButton: '#add-country',
+        editCountries: '#edit-countries',
+        dataContainer: '#dataContainer'
+    };
 
+    function GeographyApp(options) {
+        this.settings = $.extend({}, defaults, options);
+
+        this.$showCountriesButton = $(this.settings.showCountriesButton);
+        this.$addCountryButton = $(this.settings.addCountryButton);
+        this.$editCountries = $(this.settings.editCountries);
+        this.$dataContainer = $(this.settings.dataContainer);
+    }
+
+    GeographyApp.prototype.init = function () {
+        this.$showCountriesButton.on('click', $.proxy(this.getCountries, this));
+        this.$editCountries.on('click', $.proxy(this.renderEditCountries, this));
+        this.$addCountryButton.on('click', $.proxy(this.addCountry, this));
+    };
+
+    GeographyApp.prototype.getCountries = function () {
+        var _this = this;
+        $.ajax({
+            method: 'GET',
+            url: _this.settings.baseURL + '/appdata/' + _this.settings.appID + '/Countries',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Basic ' + btoa(_this.settings.username + ':' + _this.settings.pass));
+            }
+        }).done(_this.renderCountries)
+            .error(function (data) {
+                console.log(data);
+            });
+    };
+
+    GeographyApp.prototype.addCountry = function () {
+
+    };
+
+    GeographyApp.prototype.renderCountries = function (data) {
+        var countries = '';
+        for (var country in data) {
+            countries += data[country].Name + ' ';
+        }
+
+        $('#dataContainer').text(countries);
+    };
+
+    GeographyApp.prototype.renderEditCountries = function () {
+        console.log('edit');
+    };
+
+    window.GeographyApp = GeographyApp;
 
 })(jQuery, window);
